a83e147136e6822320f08d52fb3602cebfd7b76e
Björn Johansson <bjorse@gmail.com>
2017-04-03 18:17:03 +0200

    Added server messages

--- src/server.js
+++ src/server.js
@@ -20,29 +20,41 @@ const wss = new WebSocketServer({ server: server });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('Received: %s', message);
-
-    const jsonMsg = JSON.parse(message);
-
-    if (!jsonMsg.hasOwnProperty('username') || !jsonMsg.hasOwnProperty('message') ||
-        jsonMsg.username.trim() === '' || jsonMsg.message.trim() === '') {
-      return;
-    }
-
-    const userColor = jsonMsg.hasOwnProperty('userColor') && jsonMsg.userColor.trim() !== '' 
-      ? jsonMsg.userColor 
-      : '#000000';
-
-    const messageToSend = JSON.stringify({
-      username: jsonMsg.username.trim(),
-      message: jsonMsg.message.trim(),
-      userColor: userColor,
-      time: new Date().getTime()
-    });
-
-    wss.clients.forEach((client) => {
-      client.send(messageToSend);
-    });
+    sendMessageToClients(JSON.parse(message), false);
   });
 
   console.log('Client connected!');
+  sendMessageAsServer('A client has connected. How exciting!');
 });
+
+const sendMessageAsServer = (message) => {
+  sendMessageToClients({
+    username: 'Server',
+    message: message,
+    userColor: '#5e5e5e'
+  }, true);
+}
+
+const sendMessageToClients = (message, asServer) => {
+  if (!message.hasOwnProperty('username') || !message.hasOwnProperty('message') ||
+       message.username.trim() === '' || message.message.trim() === '' || (!asServer && message.username.trim().toLowerCase() === 'server')) {
+    sendMessageAsServer(`An invalid message was received: ${JSON.stringify(message)}`);
+
+    return;
+  }
+
+  const userColor = message.hasOwnProperty('userColor') && message.userColor.trim() !== '' 
+    ? message.userColor 
+    : '#000000';
+
+  const messageToSend = JSON.stringify({
+    username: message.username.trim(),
+    message: message.message.trim(),
+    userColor: userColor,
+    time: new Date().getTime()
+  });
+
+  wss.clients.forEach((client) => {
+    client.send(messageToSend);
+  });
+}
